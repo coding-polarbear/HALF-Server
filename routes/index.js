cf5524bc59e210e7c725175c49442b3067385c65
purplebeen <baehynebin@hotmail.co.kr>
2018-11-27 21:33:34 +0900

    웹소캣 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const io = require('socket.io').listen(3001);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/query', (req, res) => {
+  if(req.query.data !== undefined) {
+    io.emit('result', req.query.data);
+    res.send(req.query.data)
+  }
+});
+
+router.get('/test', (req, res) => {
+  res.render('test', {title : 'Test', io : io});
 });
 
 module.exports = router;
