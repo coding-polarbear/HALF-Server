5354c7ca6d23b38a2f1bcc3cf5e10ad2358461d1
baehyeonbin <baehynebin@hotmail.co.kr>
2018-11-28 12:27:08 +0900

    config 수정

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 "jwtAlgorithm": "HS256", // 토큰발급시 사용 알고리즘
     "algorithm": "sha256", // password 알고리즘
-    "salt": "~!@#$%^&*()_+HIGHTHON+_)(*&^%$#@!~", // salt
+    "salt": "~!@#$%^&*()_+HALF+_)(*&^%$#@!~", // salt
 };
